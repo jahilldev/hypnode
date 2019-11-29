6a87f79bf4874e80e20ba03adc00b77d5a6b05ce
James Hill <me@jhdev.uk>
2019-11-29 16:16:54 +0000

    Temporarily removed attribute definitions

--- src/attributes.ts
+++ src/attributes.ts
@@ -6,20 +6,6 @@
 
 interface IAttrs {
    [index: string]: any;
-   onClick: EventListener;
-   onMouseUp: EventListener;
-   onMouseDown: EventListener;
-   onMouseEnter: EventListener;
-   onMouseLeave: EventListener;
-   onMouseMove: EventListener;
-   onMouseOver: EventListener;
-   onMouseWheel: EventListener;
-   onMouseOut: EventListener;
-   onKeyUp: EventListener;
-   onKeyDown: EventListener;
-   onKeyPress: EventListener;
-   onChange: EventListener;
-   onSubmit: EventListener;
 }
 
 /* -----------------------------------
