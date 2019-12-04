126530880e9c9daddce0f7e390c5f4c0516bbd17
James Hill <me@jhdev.uk>
2019-12-04 12:20:27 +0000

    Removed built assets from repository

--- dist/useState.d.ts
+++
@@ -1,6 +0,0 @@
-import { IAttrs } from './attributes';
-declare type Component = (attrs?: IAttrs) => HTMLElement;
-declare function useState(initial: any): any[];
-declare function setIndex(tag: Component, attrs: IAttrs): number;
-declare function setElement(node: HTMLElement, index: number): HTMLElement;
-export { useState, setIndex, setElement };
