ab04ebba9ac9c262806a9bdd19f746b89dc37099
James Hill <me@jhdev.uk>
2020-01-31 13:15:49 +0000

    Fixed remaining indentation

--- jest.config.js
+++ jest.config.js
@@ -5,22 +5,22 @@
  * -------------------------------- */
 
 module.exports = {
-   testEnvironment: 'jsdom',
-   globals: { __DEV__: true },
-   roots: ['<rootDir>'],
-   collectCoverage: true,
-   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-   coverageDirectory: 'tests/coverage',
-   coveragePathIgnorePatterns: ['/node_modules/', '(.*).d.ts'],
-   coverageThreshold: {
-      global: {
-         statements: 97,
-         branches: 92,
-         functions: 94,
-         lines: 98,
-      },
-   },
-   transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-   },
+  testEnvironment: 'jsdom',
+  globals: { __DEV__: true },
+  roots: ['<rootDir>'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageDirectory: 'tests/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '(.*).d.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 97,
+      branches: 92,
+      functions: 94,
+      lines: 98,
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
